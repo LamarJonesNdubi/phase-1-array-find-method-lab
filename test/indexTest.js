14323ee6e73e0e60377c8021a6bf93444ac12965
Lewis Jones Ndubi <lamarjonesndubi@gmail.com>
2024-07-13 20:20:38 +0300

    adds all

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,20 @@
 require ( './helpers.js' );
 
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  
+];
+
+function superbowlWin(records) {
+  const winRecord = records.find(record => record.result === "W");
+  return winRecord ? winRecord.year : undefined;
+}
+
+console.log(superbowlWin(record)); 
+
+
 
 describe('superbowlWin(record)', function () {
   const record = [
